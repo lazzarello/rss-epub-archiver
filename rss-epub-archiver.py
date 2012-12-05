ebba12fe0faaeb37ec5e4727f3e13c7f48c1ee92
Lee Azzarello <lee@rockingtiger.com>
2012-12-05 18:43:23 -0500

    moar options, get the binary to execute

--- rss-epub-archiver.py
+++ rss-epub-archiver.py
@@ -1,6 +1,6 @@
 # import argument and option parsing
 from sys import argv
-from optparse import OptionParser
+import argparse
 # import exit codes
 from sys import exit
 # import regular expressions
@@ -15,9 +15,9 @@ import random
 # define the class to generate the conversion code for calibre
 class RecipeGenerator(object):
 
-    def __init__(self, years = 5):
-        # feed history, default is 5 years
-        self.years = years 
+    def __init__(self, days = 7):
+        # feed history, default is 7 days
+        self.days = days
         # get a random 5 digit integer
         self.rand_ext = random.randint(10000, 99999)
         # set the filename for the generated class
@@ -28,12 +28,12 @@ class RecipeGenerator(object):
         klass = """
 class BatchConversionRecipe(BasicNewsRecipe):
     title   = u'%s'
-    oldest_article = (365 * %d)
+    oldest_article = %d
     max_articles_per_feed = 100
     auto_cleanup = True
     feeds = [(u'%s', u'%s')]
 
-""" % (title, self.years, title, url)
+""" % (title, self.days, title, url)
 
         recipe = open(self.filename, 'w')
         recipe.write(klass)
@@ -64,43 +64,43 @@ installed Calibre, you must enable this utility by going to Preferences
     exit(1)
 
 # unpack arguments
-script, url = argv
-# ARGS
-# url
-
-parser = OptionParser()
-# options:
-parser.add_option("-t","--title", action="store", type="string", dest="title", default="hostname+salt")
-parser.add_option("-s", "--test", action="store_true", dest="test")
-parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
+parser = argparse.ArgumentParser(description="rip some epubs")
+parser.add_argument("url", metavar="url", nargs=1)
+parser.add_argument("-t","--title", action="store", dest="title", default="hostname+salt", help="the title of the book (default: hostname+salt)")
+parser.add_argument("-s", "--test", action="store_true", dest="test", help="run in test mode")
+parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help="verbose output for debugging")
+parser.add_argument("-d", "--days", action="store", dest="days", type=int, help="the number of days prior to start downloading posts")
+parser.add_argument("-o","--outdir", action="store", dest="outdir", default=".", help="the directory to output the final epub file (default: current dir)")
+args = parser.parse_args()
 
 # create a RecipeGenerator object and write out the file for calibre,
 # store the filename in the variable.
-recipe_file = RecipeGenerator().generate( title, url )
-
-# TODO: put all the code below into the RecipeGenerator class so it can be
-# threaded.
+recipe_file = RecipeGenerator().generate( args.title, args.url[0] )
 
 # call regex to replace all non alphanumeric chars in title with _
 # Erhhh mah gherrrd, Unicode
 # http://stackoverflow.com/questions/11689223/python-utf-8-filesystem-iso-8859-1-files
-title = re.sub("[^a-zA-Z0-9]", "_", title)
+title = re.sub("[^a-zA-Z0-9]", "_", args.title)
 
 # generate the filename of the ebook
-epub = "%s.epub" % title
+epub = "/%s.epub" % title
 
 # prepend absolute path to ebook filename
-epub = "/tmp/ebooks/%s" % epub
+epub = args.outdir + epub
 
-if ( verbose ):
+verbose = ""
+test = ""
+
+if ( args.verbose ):
     verbose = "-v -v"
 
-if ( test ):
+if ( args.test ):
     test = "--test"
 
 # pass arguments to the ebook-convert program and store the string to be
 # eval'd: JACK BAUER!
 system_string_with_args = "ebook-convert %s %s %s %s" % (recipe_file, ensure_dir(epub), verbose, test)
 
+print system_string_with_args
 # Eval the string. Do all the work!
 system(system_string_with_args)
